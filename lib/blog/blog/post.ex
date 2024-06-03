4f163f6976fefa785b67de203ab40f3d25ce04ae
LuizFerK <luizfernandoklein@live.com>
2024-06-03 20:02:10 -0300

    feat: Home screen responsiveness

--- lib/blog/blog/post.ex
+++ lib/blog/blog/post.ex
@@ -3,7 +3,6 @@ defmodule Blog.Blog.Post do
   defstruct [:id, :author, :title, :banner, :body, :description, :tags, :date]
 
   def build(filename, attrs, body) do
-    IO.inspect(attrs)
     [year, month_day_id] = filename |> Path.rootname() |> Path.split() |> Enum.take(-2)
     [month, day, id] = String.split(month_day_id, "-", parts: 3)
     date = Date.from_iso8601!("#{year}-#{month}-#{day}")
